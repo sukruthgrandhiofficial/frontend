15a2ac08a118a79f6bd8e886a12a895c38b84043
sukruth grandhi <sukruthgrandhiofficial@gmail.com>
2024-05-16 15:52:53 +0530

    setting up proper base url

--- src/script.js
+++ src/script.js
@@ -1,8 +1,3 @@
-(function (window) {
-    window.__env = window.__env || {};
-    window.__env.backendBaseURL = 'http://localhost:8001/backend1/'; // Modify this with your actual base URL
-}(this));
-
 document.addEventListener('DOMContentLoaded', function() {
     readAllItems(); // Call the function when the DOM content is loaded
 });
